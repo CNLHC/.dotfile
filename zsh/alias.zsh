f66e09ee2194300e1cf3219cd59b7ca1dbab3589
CNLHC <2463765697@qq.com>
2020-05-24 09:54:36 +0800

    add alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,1 +1,12 @@
 alias pdf="df -H -x tmpfs -x overlay --output"
+alias edit_alias="vim ~/.dotfile/zsh/alias.zsh"
+alias source_conf="source ~/.zshrc"
+alias edit_zshrc="vim ~/.zshrc"
+
+function kill_by_grep {
+     ps -aux | grep  $1 | tr -s ' '| cut -d ' ' -f2 | xargs kill -9 ;
+}
+function k8s_delete_evicted {
+    kubectl get pods -n $1| grep Evicted | tr -s ' ' | cut -d ' ' -f 1 |xargs kubectl delete pods -n $1
+}
+
