f5a0ff49fdf6066f85f58b67ecaff196e73a4436
Liu Hancheng <cn_lhc@qq.com>
2020-05-29 14:03:26 +0800

    enhance init script

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y  python\
+sudo apt-get install -y -q=2  python\
 		 python3\
 		 python-pip\
 		 python3-pip\
@@ -11,21 +11,20 @@ sudo apt install -y  python\
 		 zsh
 
 
-
 if [ ! -d ~/.config/nvim ]; then
     echo "make nvim config dir"
     mkdir -p ~/.config/nvim;
 fi
 
-echo "source-file ~/.dotfile/tmux/tmux.conf" > ~/.tmux.conf
-echo "source ~/.dotfile/vim/vimrc" > ~/.config/nvim/init.vim
 
-if [ ! -d  ~/.config/nvim/bundle/Vundle.vim ]; then
-    echo "get vundle"
-    git clone https://github.com/VundleVim/Vundle.vim ~/.config/nvim/bundle/Vundle.vim
+# install dein
+if [ ! -d  ~/.cache/dein ]; then
+    echo "get dein"
+    curl -s https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | bash /dev/stdin  ~/.cache/dein 
 else
-    echo "vundle exists"
+    echo "dein exists"
 fi
+
 
 if [ ! -d ~/.oh-my-zsh/ ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -33,13 +32,26 @@ else
     echo "oh-my-zsh exists"
 fi
 
+function check_and_add(){
+    tc=$(grep -c "$1" $2 )
 
+    if [  $tc -le 0  ]; then
+        echo "$1" >> $2
+    else
+        echo \"$1\" already exists in $2
+    fi
+}
 
-tc=$(grep -c "source ~/.dotfile/zsh/alias.zsh" ~/.zshrc )
+sed -i -E 's/plugins=[(](.+)[)]//' ~/.zshrc
+check_and_add "plugins=(git vi-mode)" ~/.zshrc
 
-if [  $tc -le 0  ]; then
-    echo "source ~/.dotfile/zsh/alias.zsh" >> ~/.zshrc
-fi
+sed -i -E 's/^# (COMPLETION_WAITING_DOTS)=(.+)$/\1=\"true\"/' ~/.zshrc
 
+check_and_add "source-file ~/.dotfile/tmux/tmux.conf" ~/.tmux.conf
+check_and_add "source ~/.dotfile/vim/vimrc" ~/.config/nvim/init.vim
+
+check_and_add "source ~/.dotfile/zsh/alias.zsh" ~/.zshrc
+check_and_add "bindkey \"^P\" up-line-or-beginning-search" ~/.zshrc
+check_and_add "bindkey \"^N\" down-line-or-beginning-search" ~/.zshrc
 
 
