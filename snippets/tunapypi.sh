cc907d7bc0cc0db03eeb28a36cdc5cb07a78729b
root <root@debug010000002015.local.alipay.net>
2021-01-12 16:18:15 +0800

    update

---
+++ snippets/tunapypi.sh
@@ -0,0 +1,7 @@
+#/bin/env bash
+
+python3 -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
+python3 -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+
+
